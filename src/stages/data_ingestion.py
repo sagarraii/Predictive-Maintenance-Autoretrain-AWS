0f484b598163c47934f9216889d47c59f5567d38
SAGA dev <sagarrai9547@gmail.com>
2026-06-01 14:48:08 +0530

    feat(pipeline): add data ingestion stage

--- src/stages/data_ingestion.py
+++ src/stages/data_ingestion.py
@@ -17,13 +17,8 @@ class DataIngestionTrainingPipeline:
 
         config = ConfigurationManager()
 
-        data_ingestion_config = (
-            config.get_data_ingestion_config()
-        )
-
-        data_ingestion = DataIngestion(
-            config=data_ingestion_config
-        )
+        data_ingestion_config = (config.get_data_ingestion_config())
+        data_ingestion = DataIngestion(config=data_ingestion_config)
 
         data_ingestion.download_data()
 
@@ -32,17 +27,12 @@ if __name__ == "__main__":
 
     try:
 
-        logger.info(
-            f">>>>>>> {STAGE_NAME} started <<<<<<"
-        )
+        logger.info(f">>>>>>> {STAGE_NAME} started <<<<<<")
 
         obj = DataIngestionTrainingPipeline()
-
         obj.initiate_data_ingestion()
 
-        logger.info(
-            f">>>>>>> {STAGE_NAME} completed <<<<<<"
-        )
+        logger.info(f">>>>>>> {STAGE_NAME} completed <<<<<<")
 
     except Exception as e:
         raise CustomException(e, sys)
