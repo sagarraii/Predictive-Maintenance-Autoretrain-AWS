febc0177ca3aad74fe6745a3f39ab88f3043c1b8
SAGA dev <sagarrai9547@gmail.com>
2026-06-01 14:35:10 +0530

    feat(main): integrate data validation pipeline

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ from src.logger.logging_config import logger
 from src.exception.custom_exception import CustomException
 
 from src.stages.data_ingestion import DataIngestionTrainingPipeline
+from src.stages.data_validation import DataValidationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
 
-try:
+def run_stage(stage_name: str, pipeline_class):
+    try:
+        logger.info(f">>>>>>> {stage_name} started <<<<<<")
+        obj = pipeline_class()
 
-        logger.info(
-            f">>>>>>> {STAGE_NAME} started <<<<<<"
-        )
+        if stage_name == "Data Ingestion Stage":
+            obj.initiate_data_ingestion()
+        elif stage_name == "Data Validation Stage":
+            obj.initiate_data_validation()
 
-        obj = DataIngestionTrainingPipeline()
+        logger.info(f">>>>>>> {stage_name} completed <<<<<<\n")
+    except Exception as e:
+        logger.exception(f"{stage_name} failed.")
+        raise CustomException(e, sys)
 
-        obj.initiate_data_ingestion()
 
-        logger.info(
-            f">>>>>>> {STAGE_NAME} completed <<<<<<"
-        )
-
-except Exception as e:
-    raise CustomException(e, sys)
+if __name__ == "__main__":
+    run_stage("Data Ingestion Stage", DataIngestionTrainingPipeline)
+    run_stage("Data Validation Stage", DataValidationTrainingPipeline)
