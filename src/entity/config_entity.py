7cc1421cfa3589d91730bf371d62e7c199917676
SAGA dev <sagarrai9547@gmail.com>
2026-06-01 14:29:53 +0530

    feat(config): add data validation configuration

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -5,3 +5,10 @@ from dataclasses import dataclass
 class DataIngestionConfig:
     data_download_dir: Path
     dataset_name: str
+
+@dataclass
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    data_file_path: Path
+    all_schema: dict
