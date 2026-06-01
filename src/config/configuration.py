9e0ae6a7a5bb8dd6c1d62fae327c7a45d8301509
SAGA dev <sagarrai9547@gmail.com>
2026-06-01 14:29:03 +0530

    feat(config): add data validation configuration

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -2,7 +2,8 @@ import os
 
 from src.constant.constants import *
 from src.utils.common import read_yaml, create_directories
-from src.entity.config_entity import (DataIngestionConfig)
+from src.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
+
 class ConfigurationManager:
     def __init__(self,
                  config_file_path = CONFIG_FILE_PATH,
@@ -14,6 +15,7 @@ class ConfigurationManager:
         self.schema = read_yaml(schema_file_path)
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
+
         config = self.config.data_ingestion
         
         data_ingestion_config = DataIngestionConfig(
@@ -22,3 +24,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+        
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            data_file_path=config.data_file_path,
+            all_schema=schema
+        )
+
+        return data_validation_config
